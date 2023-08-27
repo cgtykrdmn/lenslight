896177029727cfebe1aebbc3a6174d655a403b3a
Cagatay Karaduman <karadumancagatay@gmail.com>
2023-08-27 13:20:44 +0300

    installed packages

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js"
+
+dotenv.config();
+
+// Connect to DB
+conn();
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
+
+//ejs temp engine
+app.set("view engine", "ejs");
+
+//static file middleware
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("INDEX");
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(port, () => {
