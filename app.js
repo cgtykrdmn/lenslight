f32acb7fc0fc39d55399c07461473668c6f951eb
Cagatay Karaduman <karadumancagatay@gmail.com>
2023-08-28 13:43:42 +0300

    created mvc

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import conn from "./db.js"
+import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
 
 dotenv.config();
 
@@ -16,12 +18,9 @@ app.set("view engine", "ejs");
 //static file middleware
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
+//router
+app.use("/", pageRoute);
+app.use("/photos", photoRoute);
 
 app.listen(port, () => {
   console.log(`Application has been started at ${port}`);
