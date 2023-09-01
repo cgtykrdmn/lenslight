17068cffae4090f87930f419e108fd73305232cf
Cagatay Karaduman <karadumancagatay@gmail.com>
2023-09-01 13:44:33 +0300

    Upload Iamge, Cloudinary

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import Photo from "../models/photoModel.js"
 
 const createUser = async (req, res) => {
   try {
@@ -64,9 +65,11 @@ const loginUser = async (req, res) => {
   }
 };
 
-const getDashboardPage = (req, res) => {
+const getDashboardPage = async (req, res) => {
+  const photos = await Photo.find({user: res.locals.user._id})
   res.render("dashboard", {
     link: "dashboard",
+    photos,
   });
 };
 
